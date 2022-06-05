5a78d1e80cde7d2343b3edfc38a6c06a681aa9f0
LionelCastillo <105450241+LionelCastillo@users.noreply.github.com>
2022-06-05 13:58:42 -0300

    Add files via upload
    ejercicio 6

--- ejercicios_practica/ejercicio_6.py
+++ ejercicios_practica/ejercicio_6.py
@@ -20,9 +20,22 @@ inicio = int(input('Ingrese el primer número de la secuencia\n'))
 fin = int(input('Ingrese el último número de la secuencia\n'))
 
 cantidad_numeros_positivos = 0  # Inicializo el contador en 0
+cantidad_numero_negativos = 0 
+secuencia = range(inicio, fin +1)
 
 # for ... in range(....)
-
+for numero in secuencia:
+    if numero < 0:
+        cantidad_numero_negativos += 1
+        print('el numero es ', numero,)
+        print('son ', cantidad_numero_negativos, 'numeros negativos')
+        print('son ', cantidad_numeros_positivos, 'numeros mayores o gual a cero')
+    else:
+        cantidad_numeros_positivos += 1
+        print('el numero es ', numero)
+        print('son ', cantidad_numero_negativos, 'numeros negativos')
+        print('son ', cantidad_numeros_positivos, 'numeros mayores o iguales a cero')   
+            
 # Imprimir el valor de la cantidad de números positivos y negativos
 
 print("terminamos!")
